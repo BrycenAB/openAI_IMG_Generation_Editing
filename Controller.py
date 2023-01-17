252a28fcd6ef1ad75737b6a5fa1a2ab2f90d94a8
Brycen Bowers <86658660+BrycenAB@users.noreply.github.com>
2023-01-17 08:34:51 -0700

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -8,6 +8,10 @@ openai.api_key = "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  # API key goes her
 
 
 def create_basic_img(user_input):
+    """
+    Given a user_input, sends a request to OpenAI's API to generate an image and opens it in a new browser tab
+    Returns the url of the image
+    """
     response = openai.Image.create(
         prompt=f"{user_input}",
         n=1,
@@ -21,11 +25,18 @@ def create_basic_img(user_input):
 
 
 def show_image():
+    """
+    Calls create_basic_img() to generate an image, and then saves the image to the local system
+    """
+    
     url = create_basic_img(str(View.input_field.get()))
     save_image(url, "image")
 
 
 def save_image(url, filename):
+    """
+    Given an image URL and a filename, saves the image to the local system and converts it to png format
+    """
     response = requests.get(url)
     if response.status_code == 200:
         with open(filename, 'wb') as f:
@@ -39,6 +50,9 @@ def save_image(url, filename):
 
 
 def generate_mask(image_path, threshold):
+    """
+    Given an image and a threshold, converts the image to grayscale and creates a binary mask
+    """
     image = Image.open(image_path)
     image = image.convert("L")  # convert to grayscale
     mask = np.array(image)
@@ -51,6 +65,9 @@ def generate_mask(image_path, threshold):
 
 
 def edit_img():
+    """
+    Given the image and mask, sends a request to OpenAI's API to edit the image, and opens the new image in a new browser tab
+    """
     response = openai.Image.create_edit(
         image=open("IMGs/image.png", "rb"),
         mask=open("IMGs/mask.png", "rb"),
@@ -63,6 +80,9 @@ def edit_img():
 
 
 def reset():
+    """
+    Reset the input fields and deletes any existing images from the IMGs directory
+    """
     View.input_field.delete(0, 'end')
     View.input_field2.delete(0, 'end')
     View.threshold_entry.delete(0, 'end')
